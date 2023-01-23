63243efffc7dd61c5d2da9f644bbc04e9d5202ba
William <wvonderscher@gmail.com>
2023-01-23 10:02:36 +0100

    initial commit (ça va marcher tkt)

---
+++ src/main/java/org/miage/projet_api/ProjetApiApplication.java
@@ -0,0 +1,13 @@
+package org.miage.projet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/miage/projet_api/ProjetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miage.projet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
